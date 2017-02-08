02007c9a84bf8c3c05aefd3df386504c05d1df03
Clapinton <eric.olivetree@gmail.com>
2017-02-08 15:58:09 -0800

    Example codes from Pebble documentation

---
+++ examples/app_mockup_helper/build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_app_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleAppInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { APP_INFO_CURRENT_STRUCT_VERSION_MAJOR, APP_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { APP_INFO_CURRENT_SDK_VERSION_MAJOR, APP_INFO_CURRENT_SDK_VERSION_MINOR },
+  .app_version = { 2, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Mockup Helper",
+  .company = "Pebble Technology",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x6C, 0xC4, 0x96, 0x7F, 0xA0, 0x6E, 0x4A, 0x00, 0xBF, 0x46, 0x8A, 0x32, 0x6A, 0xCE, 0xAF, 0x31 },
+  .virtual_size = 0xb6b6
+};
